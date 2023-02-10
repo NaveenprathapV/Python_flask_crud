41ca91011b204c9157791dd357dd9ac1ac2799f5
Naveenprathap V <naveenprathap@calibraint.com>
2023-02-10 15:23:14 +0530

    made changes in env and db connections

--- app.py
+++ app.py
@@ -1,42 +1,40 @@
 from flask import Flask, request, json 
 from bson.objectid import ObjectId
-from pymongo import MongoClient
+from flask_pymongo import PyMongo
+from dotenv import load_dotenv 
+import os
 
 app = Flask(__name__)
+load_dotenv()
+app.config ['MONGO_URI']= os.environ.get('MONGO_URI')
+mongo = PyMongo(app)
 
-def createConnection():
-    connection = MongoClient("mongodb://localhost:27017")
-    db = connection.flask_db
-    return db
+# def createConnection():
+#     connection = MongoClient("mongodb://localhost:27017")
+#     db = connection.flask_db
+#     return db
 
 @app.route('/students', methods = ['GET'])
 def getStudents():
-    db = createConnection()
-    studentData = list(db.students.find({}))
-    # return studentData
-    # return json.dumps(studentData)
-    # return jsonify(list(studentData))
+    studentData = list(mongo.db.students.find({}))
     return object.__str__(studentData)
 
 @app.route('/student', methods = ['POST'])
 def createStudents():
-    db = createConnection()
     data = json.loads(request.data)
-    studentData = db.students.insert_one(data)
+    studentData = mongo.db.students.insert_one(data)
     return object.__str__(studentData)
 
 @app.route('/student/:id', methods = ['PUT'])
 def updateStudents():
-    db = createConnection()
     id = request.args.get('id')
     data = json.loads(request.data)
     print(data)
-    studentData = db.students.update_one({ '_id': ObjectId(id) }, { "$set": data })
+    studentData = mongo.db.students.update_one({ '_id': ObjectId(id) }, { "$set": data })
     return "hi"
 
 @app.route('/student/:id', methods = ['DELETE'])
 def deleteStudents():
-    db = createConnection()
     id = request.args.get('id')
     deleteQuery= {"_id": ObjectId(id)}
     
